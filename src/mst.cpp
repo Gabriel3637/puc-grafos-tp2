bee5c2a6bb8f245516e80d5733c3a2a6ddba9679
Gabriel3637 <gabriellvbs14@gmail.com>
2025-11-29 12:05:24 -0300

    alteração do edge

---
+++ src/mst.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include "graph.hpp"
+
+class mst
+{
+private:
+    std::vector<> 
+    
+public:
+    mst(/* args */);
+    ~mst();
+};
+
+mst::mst(/* args */)
+{
+}
+
+mst::~mst()
+{
+}
