b48f56483f7dcf445ed74ee68f4e80065f9aac23
Gabriel3637 <gabriellvbs14@gmail.com>
2025-11-29 21:38:17 -0300

    mst  de segmentação criada pendente de algumas modificações

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,42 @@
 #include <iostream>
 
 #include "graph.hpp"
+#include "mst.hpp"
+#include <vector>
 
-int main(void) { return 0; }
+int main(void) { 
+    std::cout << "Teste 0" << std::endl;
+    Mst testeMst = Mst();
+
+    std::cout << "Teste 1" << std::endl;
+    
+    std::vector<Edge> teste = {
+        {0, 1, 10},
+        {1, 2, 5},
+        {0, 2, 8}
+    };
+
+    for(Edge e: teste){
+        std::cout << e << std::endl;
+    }
+
+
+
+    std::cout << "Teste 2" << std::endl;
+
+    std::vector<size_t> agm = testeMst.generateMST(teste, 3, 10);
+
+    std::cout << "Teste 3" << std::endl;
+/* 
+    for(Edge e: agm){
+        std::cout << e << std::endl;
+    } */
+
+    std::cout << "Teste 4" << std::endl;
+
+
+
+
+    return 0; 
+
+}
