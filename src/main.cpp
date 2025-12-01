fb9bffd139a2486854c2a978e5e9965b7ed1d9a1
HenriqueGiberti <henriquegiberti@gmail.com>
2025-11-30 22:06:34 -0300

    umaBostaMasTaAí

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,10 @@
 
 #include <iostream>
 
-#include "../inc/graphDir.hpp" 
+#include "../inc/graphDir.hpp"
+#include <unordered_map>
+#include <array>
+#include <cstdlib> 
 
 double euclidiana(unsigned char *img, int i, int j) {
     int ri = img[i * 3 + 0];
@@ -26,9 +29,10 @@ double euclidiana(unsigned char *img, int i, int j) {
     return dist;
 }
 
-void segDG(std::string im) { 
+int main(void) { 
     
      int width=2, height=2, channels = 3;
+     std::string im = "imgs/Chiaki_Nanami_Hoi4.png";
     unsigned char *img =
         stbi_load(im.c_str(), &width, &height, &channels, 3);
     
@@ -45,7 +49,7 @@ void segDG(std::string im) {
         if((i/3)%width > 0) maxDist = std::max(maxDist, euclidiana(img, i/3, i/3-1));
         if(i/3/width > 0) maxDist = std::max(maxDist, euclidiana(img, i/3, i/3-width));
     }
-    double gamma = maxDist * 2;
+    double gamma = maxDist * 1.5;
     
     Graph image = Graph(width*height);
     for(int i=0;i<width*height*3;i=i+3){
@@ -61,7 +65,7 @@ void segDG(std::string im) {
         }
     }
 
-    //std::cout << "Image: \n" << image.toString() << std::endl;
+    std::cout << "Image: \n" << image.toString() << std::endl;
 
     //adiciono vértice ligando a todo mundo:
     Graph image2 = Graph(width*height+1);
@@ -77,10 +81,12 @@ void segDG(std::string im) {
         image2.addEdge(width*height, i, gamma);
     }
 
-    //std::cout << "Image2: \n" << image2.toString() << std::endl;
+    std::cout << "Image2: \n" << image2.toString() << std::endl;
+
+    std::cout << "abc";
 
     Graph gab = image2.optimumBranchingGabow(width*height);
-    //std::cout << "MSA: \n" << gab.toString() << std::endl;
+    std::cout << "MSA: \n" << gab.toString() << std::endl;
 
     Graph image3 = Graph(width*height);
     for (int u = 0; u < width*height; u++) {
@@ -89,14 +95,68 @@ void segDG(std::string im) {
         }
     }
 
-    //std::cout << "Image3: \n" << image3.toString() << std::endl;
+    std::cout << "Image3: \n" << image3.toString() << std::endl;
 
     std::vector<int> fracamente;
     fracamente = image3.componentesFracamenteConexos();
 
-    for(int i : fracamente){
-        //std::cout<< "[" <<fracamente[i] << "] ";
-    }  
+    for (int idx = 0; idx < fracamente.size(); idx++) {
+        std::cout << fracamente[idx] << " ";
+    }
+
+    struct Acc {
+        long r = 0, g = 0, b = 0;
+        int count = 0;
+    };
+
+    std::unordered_map<int, Acc> acc;
+
+    for (int i = 0; i < width * height; i++) {
+        int comp = fracamente[i];
+
+        acc[comp].r += img[i*3 + 0];
+        acc[comp].g += img[i*3 + 1];
+        acc[comp].b += img[i*3 + 2];
+        acc[comp].count++;
+    }
+
+    std::unordered_map<int, std::array<unsigned char,3>> corMedia;
+
+    for (auto &p : acc) {
+        corMedia[p.first] = {
+            (unsigned char)(p.second.r / p.second.count),
+            (unsigned char)(p.second.g / p.second.count),
+            (unsigned char)(p.second.b / p.second.count)
+        };
+    }
+
+    unsigned char* out =
+        (unsigned char*)malloc(width * height * 3);
+
+    for (int i = 0; i < width * height; i++) {
+        auto c = corMedia[fracamente[i]];
+
+        out[i*3 + 0] = c[0];
+        out[i*3 + 1] = c[1];
+        out[i*3 + 2] = c[2];
+    }
+
+    stbi_write_png(
+        "imgs/segmentada_media.png",
+        width,
+        height,
+        3,
+        out,
+        width * 3
+    );
+
+    free(out);
+
+
+
+
 
     stbi_image_free(img);
+
+    return 0;
 }
